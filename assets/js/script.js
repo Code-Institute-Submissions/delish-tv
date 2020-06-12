2e8f31da2c4b9a9a33483bf65e4f5418c7d47d2e
mark-obeirne <markobeirne00@gmail.com>
2020-06-12 16:39:47 +0000

    Add basic map with Breakfast, Lunch and Dinner locations

---
+++ assets/js/script.js
@@ -0,0 +1,79 @@
+// key: AIzaSyAKKq48oILIJR-D3a22vDaJXZ8-4glyBrE
+
+function initMap() {
+    let map = new google.maps.Map(document.querySelector("#map"), {
+                zoom: 2,
+                center: {
+                    lat: 46.619261,
+                    lng: -33.134766
+                }
+                });
+
+    let labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let locations = [ 
+            {
+                lat: 49.2722501,
+                lng: -123.1352766
+            },
+            {
+                lat: 49.1780648,
+                lng: -123.1377265
+            },
+            {
+                lat: 49.2243079,
+                lng: -123.0928184
+            },
+            {
+                lat: 49.1797407,
+                lng: -123.1400272
+            },
+            {
+                lat: 31.6269935,
+                lng: -7.9898378
+            },
+            {
+                lat: 31.6330789,
+                lng: -8.0122642
+            },
+            {
+                lat: 33.9725228,
+                lng: -118.4280101
+            },
+            {
+                lat: 34.0905441,
+                lng: -118.3467855
+            },
+            {
+                lat: 33.88804,
+                lng: -118.2944077
+            },
+            {
+                lat: 33.8847021,
+                lng: -118.3111358
+            },
+            {
+                lat: 33.8804593,
+                lng: -118.3115567
+            },
+            {
+                lat: 11.5761988,
+                lng: 104.9161599
+            },
+            {
+                lat: 11.5695572,
+                lng: 104.9187193
+            },
+            {
+                lat: 11.5531691,
+                lng: 104.9268585
+            }
+        ];
+                let markers = locations.map((location, i) => {
+            return new google.maps.Marker({
+                position: location,
+                label: labels[i % labels.length]
+            });
+        });
+        let markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+};
