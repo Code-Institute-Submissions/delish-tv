7c611edbb3ee9970d70eb3cd2515ca5c47c60947
mark-obeirne <markobeirne00@gmail.com>
2020-06-13 07:51:08 +0000

    Update infoWindowContent to set unique string per marker

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,6 +55,132 @@ let restaurants = [
         episode: 1,
         episodeLink: "",
         website: ""
+    },
+    {
+        name: "Chez Lamine Hadj Mustapha",
+        address1: "Derb Semmarine",
+        city: "Marrakech",
+        country: "Morocco",
+        postcode: "40000",
+        lat: 31.6269935,
+        lng: -7.9898378,
+        series: "Breakfast, Lunch & Dinner",
+        season: 1,
+        episode: 2,
+        episodeLink: "",
+        website: ""
+    },
+    {
+        name: "Grand Café de la Poste",
+        address1: "Angle Boulevard El Mansour Eddahbi et Avenue Imam،",
+        city: "Marrakech",
+        country: "Morocco",
+        postcode: "40000",
+        lat: 31.6330789,
+        lng: -8.0122642,
+        series: "Breakfast, Lunch & Dinner",
+        season: 1,
+        episode: 2,
+        episodeLink: "",
+        website: "http://www.grandcafedelaposte.restaurant/"
+    },
+    {
+        name: "Winsome",
+        address1: "6080 Center Dr",
+        city: "Los Angeles",
+        country: "United States",
+        postcode: "CA 90045",
+        lat: 33.9725228,
+        lng: -118.4280101,
+        series: "Breakfast, Lunch & Dinner",
+        season: 1,
+        episode: 3,
+        episodeLink: "",
+        website: "https://www.eatwinsome.com/"
+    },
+    {
+        name: "Hot n Juicy Crawfish",
+        address1: "7100 Santa Monica Blvd #150",
+        city: "West Hollywood",
+        country: "United States",
+        postcode: "CA 90046",
+        lat: 34.0905441,
+        lng: -118.3467855,
+        series: "Breakfast, Lunch & Dinner",
+        season: 1,
+        episode: 3,
+        episodeLink: "",
+        website: "http://hotnjuicycrawfish.com/"
+    },
+    {
+        name: "Gardena Bowl Coffee Shop",
+        address1: "15707 S Vermont Ave",
+        city: "Gardena",
+        country: "United States",
+        postcode: "CA 90247",
+        lat: 33.88804,
+        lng: -118.2944077,
+        series: "Breakfast, Lunch & Dinner",
+        season: 1,
+        episode: 3,
+        episodeLink: "",
+        website: "http://gardenabowlcoffeeshop.com/"
+    },
+    {
+        name: "Chikara Mochi",
+        address1: "16108 S Western Ave",
+        city: "Gardena",
+        country: "United States",
+        postcode: "CA 90247",
+        lat: 33.8847021,
+        lng: -118.3111358,
+        series: "Breakfast, Lunch & Dinner",
+        season: 1,
+        episode: 3,
+        episodeLink: "",
+        website: ""
+    },
+    {
+        name: "Otafuku Noodle House",
+        address1: "16525 S Western Ave",
+        city: "Gardena",
+        country: "United States",
+        postcode: "CA 90247",
+        lat: 33.8804593,
+        lng: -118.3115567,
+        series: "Breakfast, Lunch & Dinner",
+        season: 1,
+        episode: 3,
+        episodeLink: "",
+        website: "http://places.singleplatform.com/otofuku-noodle-house/menu"
+    },
+    {
+        name: "Raffles Hotel Le Royal",
+        address1: "87 Samdach Sang Neayok Srey St.",
+        city: "Phnom Penh",
+        country: "Cambodia",
+        postcode: "",
+        lat: 11.5695572,
+        lng: 104.9187193,
+        series: "Breakfast, Lunch & Dinner",
+        season: 1,
+        episode: 4,
+        episodeLink: "",
+        website: ""
+    },
+    {
+        name: "Malis Restaurant",
+        address1: "Street 41",
+        city: "Phnom Penh",
+        country: "Cambodia",
+        postcode: "",
+        lat: 11.5531691,
+        lng: 104.9268585,
+        series: "Breakfast, Lunch & Dinner",
+        season: 1,
+        episode: 4,
+        episodeLink: "",
+        website: ""
     }
 ];
 
@@ -70,7 +196,7 @@ function initMap() {
     let map = new google.maps.Map(document.querySelector("#map"), mapDefaults);
 
     let labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let locations = [ 
+    /* let locations = [ 
             {
                 lat: restaurants[0].lat,
                 lng: restaurants[0].lng
@@ -127,24 +253,50 @@ function initMap() {
                 lat: 11.5531691,
                 lng: 104.9268585
             }
-        ];
+        ]; */
 
-        let infowindow = new google.maps.InfoWindow({
+        
+        let infoWindowContent = [];
+        for (let i = 0; i < restaurants.length; i++) {
+            infoWindowContent[i] = getInfoWindowDetails(restaurants[i]);
+            let location = new google.maps.LatLng(restaurants[i].lat,restaurants[i].lng);
+            marker = new google.maps.Marker({
+                position: location,
+                map: map,
+                label: labels[i % labels.length]
+            });
+
+            google.maps.event.addListener(marker, "click", (function(marker, i) {
+                return function() {
+                    infoWindow.setContent(infoWindowContent[i]);
+                    infoWindow.open(map, marker);
+                }
+            })(marker, i));
+        }
+
+        function getInfoWindowDetails(location) {
+            let infoContentString = "<div class='map-info'>" + restaurants[i].name + "</div>";
+            return infoContentString
+        }
+        /*
+        let infoWindowContent = new google.maps.InfoWindow({
             content: "<div class='map-info'>" + restaurants[0].name + "</div>"
         })
-
-                let markers = locations.map((location, i) => {
+*/
+        /*        let markers = locations.map((location, i) => {
             return new google.maps.Marker({
                 position: location,
                 label: labels[i % labels.length]
-            });
-        });
-        let markerCluster = new MarkerClusterer(map, markers,
+            }); 
+        });*/
+        
+        
+        /* let markerCluster = new MarkerClusterer(map, markers,
             {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
             
             markers.forEach((marker) => {
                 marker.addListener("click", function() {
                 infowindow.open(map, marker);
             });
-        });
+        }); */
     };
